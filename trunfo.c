87303a22efc4d84a557cdcd980466c16715b4fb7
Arthurito-TF <arthurtumelero11@gmail.com>
2025-02-26 17:50:26 -0300

    Update trunfo.c

--- trunfo.c
+++ trunfo.c
@@ -4,12 +4,13 @@ int main(){
 
     //Super Trunfo
     
-    char estado, estado2;
+    char estado;
+    char estado2;
     char codigo[50];
     char codigo2[50];
     char cidade2[50];
     char cidade[50];
-    int populaçao, populaçao2;
+    int populacao, populacao2;
     int turistico, turistico2;
     float area, area2;
     float pib, pib2;
@@ -17,7 +18,7 @@ int main(){
     //Informações da Carta 1
 
     printf("Digite o estado(Ex: A, B, C) \n");
-    scanf("%s", &estado);
+    scanf("%c", &estado);
 
     printf("Digite o código da carta(Ex: A1, A2, A3): \n");
     scanf("%s", &codigo);
@@ -26,7 +27,7 @@ int main(){
     scanf("%s", &cidade);
 
     printf("Digite a população: \n");
-    scanf("%d", &populaçao);
+    scanf("%d", &populacao);
 
     printf("Digite a área2: \n");
     scanf("%f", &area);
@@ -35,12 +36,12 @@ int main(){
     scanf("%f", &pib);
 
     printf("Digite a quantidade de pontos turísticos: \n");
-    scanf("%d", &turistico);
+    scanf("%d", &turistico); 
 
     //Informações Carta 2
 
     printf("Digite o estado(Ex: A, B, C) \n");
-    scanf("%s", &estado2);
+    scanf("%c", &estado2);
 
     printf("Digite o código da carta(Ex: A1, A2, A3): \n");
     scanf("%s", &codigo2);
@@ -49,7 +50,7 @@ int main(){
     scanf("%s", &cidade2);
 
     printf("Digite a população: \n");
-    scanf("%d", &populaçao2);
+    scanf("%d", &populacao2);
 
     printf("Digite a área2: \n");
     scanf("%f", &area2);
@@ -65,10 +66,10 @@ int main(){
 
     printf("Carta 1 \n");
 
-    printf("Estado: %s\n", estado);
+    printf("Estado: %c\n", estado);
     printf("Código: %s\n", codigo);
     printf("Nome da Cidade: %s\n", cidade);
-    printf("População: %d\n", populaçao);
+    printf("População: %d\n", populacao);
     printf("Área: %f\n", area);
     printf("PIB: %f\n", pib);
     printf("Número de Pontos Turísticos: %d \n", turistico);
@@ -77,10 +78,10 @@ int main(){
 
     printf("Carta 2 \n");
 
-    printf("Estado: %s\n", estado2);
+    printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", cidade2);
-    printf("População: %d\n", populaçao2);
+    printf("População: %d\n", populacao2);
     printf("Área: %f\n", area2);
     printf("PIB: %f\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", turistico2);
@@ -88,7 +89,7 @@ int main(){
 
 
 
-    
+    return 0;
 
 
 
